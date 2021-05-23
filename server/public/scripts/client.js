5236763563c054b4a2ab8f1818615f5f634cd3e4
pthomas0309 <preston.thomas355@gmail.com>
2021-05-23 00:42:21 -0500

    client appends calculations to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,17 +3,17 @@ $(readyNow);
 //empty array to hald the calculations
 let calcQuery = [];
 //ready now handles click listeners
-function readyNow(){
+function readyNow() {
     console.log('jQ');
     //click listener for the submit/equals button
     $('#addition').on('click', determineOperator);
     $('#subtraction').on('click', determineOperator);
     $('#division').on('click', determineOperator);
     $('#multiplication').on('click', determineOperator);
-    $('#doMath').on('click', queueCalculation)
+    $('#doMath').on('click', queueCalculation);
 }
 
-function serveCalculations(){
+function serveCalculations() {
     //plan a GET route from the server to pull the array of calculations to client side
     $.ajax({
         method: 'GET',
@@ -21,11 +21,20 @@ function serveCalculations(){
     }).then(response => {
         //check the res.send
         console.log(response);
+        //empty the DOM
+        $('#answer').empty();
+        $('#calcHistory').empty();
+        //append calculations to the DOM
+        $('#answer').append(`<p><strong>${response[0].calcAnswer}</strong></p>`);
+        //loop through array backwards for chronollogy
+        for (let i = response.length - 1; i >= 0; i--) {
+            $('#calcHistory').append(`<li>${response[i].inputOne} ${response[i].operator} ${response[i].inputTwo} = <strong>${response[i].calcAnswer}</strong></li>`);
+        };
     });
 }
 
 //create an object with the method of calculation
-function determineOperator(){
+function determineOperator() {
     //create variable for the operatorType data on button
     let currentOperator = $(this).data('operator');
     console.log('clicked', currentOperator);
@@ -41,8 +50,8 @@ function determineOperator(){
     console.log(calcQuery);
 }
 
-function queueCalculation(){
-    for (let calculation of calcQuery){
+function queueCalculation() {
+    for (let calculation of calcQuery) {
         //create inputOne property and set it equal to the value of the first input
         calculation.inputOne = $('#integerOne').val();
         //create inputTwo property and set it equal to the value of the second input
@@ -51,7 +60,7 @@ function queueCalculation(){
     }
     //var to access the calcQuery
     const calculation = calcQuery[0];
-     //post the calculation query to the server side container
+    //post the calculation query to the server side container
     $.ajax({
         method: 'POST',
         //route to server
@@ -62,6 +71,6 @@ function queueCalculation(){
         //check response
         console.log(response);
     })
-     //make a promise to the server
-     serveCalculations();
+    //make a promise to the server
+    serveCalculations();
 }
--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-const PORT = 5000; 
+const PORT = 5000;
 
 //storage for user calculation history
 const calculationCollection = [];
 //take the object values and decide what to do with them
-function calculateNumbers(){
+function calculateNumbers() {
     //conditional accounts for the operator property of the new object being + (addition)
-    if (calculationCollection[0].operator === '+'){
+    if (calculationCollection[0].operator === '+') {
         let answer = Number(calculationCollection[0].inputOne) + Number(calculationCollection[0].inputTwo);
         console.log(answer);
         calculationCollection[0].calcAnswer = answer;
     }
     //repeat for subtraction (-)
-    if (calculationCollection[0].operator === '-'){
+    if (calculationCollection[0].operator === '-') {
         let answer = Number(calculationCollection[0].inputOne) - Number(calculationCollection[0].inputTwo);
         console.log(answer);
         calculationCollection[0].calcAnswer = answer;
     }
     //repeat for multiplication (*)
-    if (calculationCollection[0].operator === '*'){
+    if (calculationCollection[0].operator === '*') {
         let answer = Number(calculationCollection[0].inputOne) * Number(calculationCollection[0].inputTwo);
         console.log(answer);
         calculationCollection[0].calcAnswer = answer;
     }
     //repeat for division (/)
-    if (calculationCollection[0].operator === '/'){
+    if (calculationCollection[0].operator === '/') {
         let answer = Number(calculationCollection[0].inputOne) / Number(calculationCollection[0].inputTwo);
         console.log(answer);
         calculationCollection[0].calcAnswer = answer;
@@ -36,12 +36,12 @@ function calculateNumbers(){
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET & POST Routes go here
 
 //POST route takes input query from client side for calculation
-app.post('/calculations', (req,res) => {
+app.post('/calculations', (req, res) => {
     //check the client info
     console.log(req.body);
     //push into the server side array
@@ -57,10 +57,10 @@ app.get('/calculations', (req, res) => {
     //do the calculation on the object values
     calculateNumbers();
     //respond with array of calculations
-    res.send(calculationCollection)
+    res.send(calculationCollection);
 });
 
 //port listener
 app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
-  });
+    console.log('Server is running on port', PORT);
+});
